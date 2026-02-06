0d311e9a0e94651be05a97607a00cf0f43f5fb82
AdrianPalmadev <adrianmilwid@gmail.com>
2026-02-06 19:10:26 +0100

    Fix Rename purchase history columns to correct values Fixes #2 issue

--- resources/views/user/purchase_history.blade.php
+++ resources/views/user/purchase_history.blade.php
@@ -264,8 +264,8 @@ input[type=submit]:hover {
                 <th>Ticketing Date & Time</th>
                 <th>Train Number</th>
                 <th>Train Name</th>
-                <th>Form</th>
-                <th>TO</th>
+                <th>City departure</th>
+                <th>City destination</th>
                 <th>Class</th>
                 <th>Seat Number</th>
                 <th>Journey Date & Time</th>
